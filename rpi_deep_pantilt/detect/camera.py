9414b301afc1b3579760dc10ead64abe4ca03fc6
Mohamed Farghaly <s-mohamed.fargaly@zewailcity.edu.eg>
2020-08-06 15:13:58 +0200

    pycharm 3

--- rpi_deep_pantilt/detect/camera.py
+++ rpi_deep_pantilt/detect/camera.py
@@ -183,9 +183,14 @@ class WebcamVideoStream:
                 return
 
             # otherwise, read the next frame from the stream
-            # cv2.imshow("Feed", self.overlay)
-            # cv2.waitKey(1)
-            print(self.overlay)
+            if self.overlay:
+                cv2.imshow("Feed", self.overlay)
+                cv2.waitKey(1)
+                print(np.shape(self.overlay))
+            else:
+                cv2.imshow("Feed", self.resized)
+                cv2.waitKey(1)
+
     def read(self):
         # return the frame most recently read
         self.resized = cv2.resize(self.frame, (320, 320))
